ea844394b0d72d7eaabdf79921b51fafa6d2f8ec
Robert Munro <rob@sentinelweb.co.uk>
2018-06-30 23:49:25 +0100

    output of codelabs

--- tf_poets_1/tensorflow-for-poets-2/android/tflite/app/src/main/java/com/example/android/tflitecamerademo/ImageClassifier.java
+++ tf_poets_1/tensorflow-for-poets-2/android/tflite/app/src/main/java/com/example/android/tflitecamerademo/ImageClassifier.java
@@ -43,10 +43,10 @@ public class ImageClassifier {
   private static final String TAG = "TfLiteCameraDemo";
 
   /** Name of the model file stored in Assets. */
-  private static final String MODEL_PATH = "graph.lite";
+  private static final String MODEL_PATH = "optimized_graph_train.lite";
 
   /** Name of the label file stored in Assets. */
-  private static final String LABEL_PATH = "labels.txt";
+  private static final String LABEL_PATH = "retrained_labels_train.txt";
 
   /** Number of results to show in the UI. */
   private static final int RESULTS_TO_SHOW = 3;
